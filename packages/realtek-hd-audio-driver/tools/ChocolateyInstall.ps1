9191afe9332fd3557b1865349432c4ae62b93957
Piotr Stankowski <git@trakos.pl>
2015-12-08 09:19:40 +0100

    realtek-hd-audio-driver

---
+++ packages/realtek-hd-audio-driver/tools/ChocolateyInstall.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'IniParser.psm1')
+
+$packageName = 'realtek-hd-audio-driver'
+$installerType = 'exe'
+$32BitUrl = 'http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/pc/audio/0006-32bit_Win7_Win8_Win81_Win10_R279.exe'
+$64BitUrl = 'http://12244.wpc.azureedge.net/8012244/drivers/rtdrivers/pc/audio/0006-64bit_Win7_Win8_Win81_Win10_R279.exe'
+$setupConfig = Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) "..\setup.iss"
+$silentArgs = ('/s /a /s /sms /f1' + $setupConfig)
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" "$64BitUrl" -validExitCodes $validExitCodes
+
+$setupLog = Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) "..\setup.log"
+$logData = IniParser-ParseFile($setupLog)
+$resultCode = $logData['ResponseResult']['ResultCode']
+if (-not ($resultCode -eq 0)) {
+	Throw "Installation ResultCode was $($resultCode), expected 0!"
+}
+echo "You need to restart your computer for the driver to work."
---
+++ packages/realtek-hd-audio-driver/tools/IniParser.psm1
@@ -0,0 +1,24 @@
+Function IniParser-ParseFile($file) {
+  $ini = @{}
+
+ # Create a default section if none exist in the file. Like a java prop file.
+ $section = "NO_SECTION"
+ $ini[$section] = @{}
+
+  switch -regex -file $file {
+    "^\[(.+)\]$" {
+      $section = $matches[1].Trim()
+      $ini[$section] = @{}
+    }
+    "^\s*([^#].+?)\s*=\s*(.*)" {
+      $name,$value = $matches[1..2]
+      # skip comments that start with semicolon:
+      if (!($name.StartsWith(";"))) {
+        $ini[$section][$name] = $value.Trim()
+      }
+    }
+  }
+  $ini
+}
+
+Export-ModuleMember IniParser-ParseFile
