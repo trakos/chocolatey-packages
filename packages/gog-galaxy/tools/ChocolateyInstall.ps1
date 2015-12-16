aa08c20d7fae236c96d70c10c9bc5a6f516830c1
Piotr Stankowski <git@trakos.pl>
2015-12-16 19:43:40 +0100

    Added gog-galaxy package

---
+++ packages/gog-galaxy/tools/ChocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿
+$packageName = 'gog-galaxy'
+$installerType = 'exe'
+$32BitUrl = 'http://cdn.gog.com/open/galaxy/client/setup_galaxy_1.1.5.28.exe'
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$32BitUrl" -validExitCodes $validExitCodes
